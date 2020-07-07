4a6a171e6425d714a3d77abae4e7b39667223db4
licyafighting@gmail.com <licyafighting@gmail.com>
2020-07-07 23:24:29 +0800

    123

---
+++ shop-parent/shop-common/src/main/java/com/tom/exception/CustomerException.java
@@ -0,0 +1,15 @@
+package com.tom.exception;
+
+import com.tom.constant.ShopCode;
+
+/**
+ * 自定义异常
+ */
+public class CustomerException extends RuntimeException{
+
+    private ShopCode shopCode;
+
+    public CustomerException(ShopCode shopCode) {
+        this.shopCode = shopCode;
+    }
+}
